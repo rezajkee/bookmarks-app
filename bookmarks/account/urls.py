47b2c71feb729196c1028fca6e5f007dc3db55a9
Anton Shefer <rezajkee@mail.ru>
2022-12-06 22:20:07 +0500

    Add password change views

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     # login / logout url
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    # change password views
+    path("password-change/", auth_views.PasswordChangeView.as_view(), name="password_change"),
+    path("password-change/done/", auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
     path("", views.dashboard, name="dashboard"),
 ]
