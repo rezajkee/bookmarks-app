77ecb34875cc6c17b12569eb8ef0cd0c75aeca3d
Anton Shefer <rezajkee@mail.ru>
2022-12-06 20:47:20 +0500

    Change login view to CBV, add logout and dashboard

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -129,3 +129,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Login/logout urls
+
+LOGIN_REDIRECT_URL = "dashboard"
+LOGIN_URL = "login"
+LOGOUT_URL = "logout"
