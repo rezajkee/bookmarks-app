1d138590cbdfc0b8c17f16020df53d7002ac108e
Anton Shefer <rezajkee@mail.ru>
2022-12-06 23:46:18 +0500

    Add registration

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from .forms import LoginForm, UserRegistrationForm
 
 
 def user_login(request):
@@ -11,9 +12,7 @@ def user_login(request):
         if form.is_valid():
             cd = form.cleaned_data
             user = authenticate(
-                request,
-                username=cd["username"],
-                password=cd["password"]
+                request, username=cd["username"], password=cd["password"]
             )
             if user is not None:
                 if user.is_active:
@@ -31,3 +30,16 @@ def user_login(request):
 @login_required
 def dashboard(request):
     return render(request, "account/dashboard.html", {"section": "dashboard"})
+
+
+def register(request):
+    if request.method == "POST":
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data["password"])
+            new_user.save()
+            return render(request, "account/register_done.html", {"new_user": new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, "account/register.html", {"user_form": user_form})
