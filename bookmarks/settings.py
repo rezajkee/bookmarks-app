1012bfaef7b017397e11b6247ac9cf5bd7c20c9e
Anton Shefer <rezajkee@mail.ru>
2022-12-09 17:44:20 +0500

    Add list of users page and detail user page

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+from django.urls import reverse_lazy
 
 from dotenv import load_dotenv
 
@@ -176,3 +177,9 @@ SOCIAL_AUTH_PIPELINE = [
     "social_core.pipeline.social_auth.load_extra_data",
     "social_core.pipeline.user.user_details",
 ]
+
+# User's absolute URL override
+
+ABSOLUTE_URL_OVERRIDES = {
+    "auth.user": lambda u: reverse_lazy("user_detail", args=[u.username])
+}
