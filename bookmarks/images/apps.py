51676e3c2660fe69aff3062daef17fcc42edd595
Anton Shefer <rezajkee@mail.ru>
2022-12-10 21:24:40 +0500

    Denormalize Image model by adding total_likes counter, add signal for m2m_change counting

--- bookmarks/images/apps.py
+++ bookmarks/images/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ImagesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "bookmarks.images"
+
+    def ready(self):
+        # Import signal handlers
+        import bookmarks.images.signals  # NOQA: F401
