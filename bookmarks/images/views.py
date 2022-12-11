991eb67ae97af6c490416b13260218330e3321e1
Anton Shefer <rezajkee@mail.ru>
2022-12-11 22:05:36 +0500

    Add images ranking functionality via Redis

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,22 +1,19 @@
+import redis
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.http import require_POST
-import redis
-from django.conf import settings
 
 from ..actions.utils import create_action
 from .forms import ImageCreateForm
 from .models import Image
 
-
 # Connect to Redis
 r = redis.Redis(
-    host=settings.REDIS_HOST,
-    port=settings.REDIS_PORT,
-    db=settings.REDIS_DB
+    host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB
 )
 
 
@@ -47,8 +44,12 @@ def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     # Increment total image views by 1
     total_views = r.incr(f"image:{image.id}:views")
+    # Increment image ranking by 1
+    r.zincrby("image_ranking", 1, image.id)
     return render(
-        request, "images/image/detail.html", {"selection": "images", "image": image, "total_views": total_views}
+        request,
+        "images/image/detail.html",
+        {"selection": "images", "image": image, "total_views": total_views},
     )
 
 
@@ -97,4 +98,19 @@ def image_list(request):
         )
     return render(
         request, "images/image/list.html", {"section": "images", "images": images}
+    )
+
+
+@login_required
+def image_ranking(request):
+    # Get image ranking dictionary
+    image_ranking = r.zrange("image_ranking", 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    # Get most viewed images
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(
+        request,
+        "images/image/ranking.html",
+        {"section": "images", "most_viewed": most_viewed},
     )
